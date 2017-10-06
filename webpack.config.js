60a62a2986f5050118713369475d34379a41bbaa
Junlong Wang <wjlinnz@gmail.com>
2017-10-06 19:42:21 +1300

    Init project; Using Webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+	template: './client/index.html',
+	filename: 'index.html',
+	inject: 'body'
+})
+
+
+module.exports ={
+	entry:'./client/index.js', //entry file where the bundler starts the bundling process.
+	output: { //location where the bundled Javascript code is to be saved.
+		path: path.resolve('dist'),
+		filename: 'index_bundle.js',
+	},
+
+	module: {
+		loaders:[ //To interprate JSX, JS; transformations that are applied on a file in our app. 
+			{test:/\.js$/, loader: 'babel-loader', exclude: /node_modules/},
+			{
+				test:/\.jsx$/, loader: 'babel-loader', exclude: /node_modules/
+			},
+		],
+	},
+
+	plugins: [HtmlWebpackPluginConfig]
+};
+
