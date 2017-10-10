58eb456befd937826102edc4d87bfd69631718b6
Junlong Wang <wjlinnz@gmail.com>
2017-10-11 00:58:02 +1300

    Fixed 404 for typed url: Return index.html for all browser user typed url

---
+++ client_server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const port = process.env.PORT||8000;
+// import {renderToString} from 'react-dom/server';
+// import  App from './client/components/App';
+// import React from 'react';
+
+
+const app = express();
+
+app.use(express.static(__dirname+'/dist'));
+
+
+app.use('/*', (req,res)=>{
+	console.log("Typed url handled");
+	res.sendFile(__dirname+'/dist/index.html');
+});
+
+app.listen(port, ()=>{
+	console.log(`PORT ${port} listening`);
+})
+
+
+module.exports=app;
