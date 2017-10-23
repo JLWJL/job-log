e580f5801d0e38b51bdac6022c4c0b3a6ee239c4
Junlong Wang <wjlinnz@gmail.com>
2017-10-23 16:23:09 +1300

    Refactor-AuthComponent: Moved out routes, moved in error boundary - Moved out routes along with regarding 'import' - Added 'componentDidCatch' for error handling - Updated 'state' property for error handling

--- client/components/AuthComponent.js
+++ client/components/AuthComponent.js
@@ -1,56 +1,62 @@
 import React from 'react';
-import {NavLink,Route,IndexRoute, Switch, Redirect} from 'react-router-dom';
 
 import AuthService from '../services/AuthService';
+
 import Header from './Header';
 import Main from './Main';
-import Account from './Account';
-import JobsOverview from './jobs/JobsOverview.js';
-import HomePage from './HomePage';
-import HelpPage from './HelpPage';
-import NotFoundPage from './NotFoundPage';
+
 
 export default class AuthComponent extends React.Component{
 	constructor(props){
 		super(props);
 		this.state={
 			isLoggedIn:false,
-		};
+		}
 
 		this.Auth = new AuthService();
 	}
 
-	componentWillMount(){
+	componentDidMount(){
 		//Check login
 		if(this.Auth.isLoggedIn()){
 			this.setState({
 				isLoggedIn:true,
+				hasError:true,
+				error: "",
+				info: ""
 			})
 		}
 	}
 
+	componentDidCatch(error, info){
+		this.setState({
+			hasError:true,
+			error: error,
+			info: info
+		})
+	}
+
 
 	render(){
-		const isLoggedIn = this.state.isLoggedIn;
+		const {isLoggedIn, hasError, error, info} = this.state;
 		return(
 			<div className="app">
 				<Header isLoggedIn={isLoggedIn} />
-			
-				<Main>
-					<Switch>
-						<Route exact path='/' component={HomePage}/>
-						<Route path='/help' component={HelpPage}/>
+				
+				{hasError?
+					(
+					<div className="error-promps">
+						<h1>Opps, Error!</h1>
+						<details style={{whiteSpace:'pre-wrap'}}>
+						 {error && error.toString()}
+						 <br/>
+						{info.componentStack}
+						</details>
+					</div>
+				):(
+					<Main isLoggedIn={isLoggedIn}/>
+				)}
 
-						<Route path='/jobs' component={JobsOverview}/>
-
-						<Route path='/account' render={props=>(
-								<Account isLoggedIn={isLoggedIn} />
-							)
-						}/>
-						
-						<Route path='/*' component={NotFoundPage}/>
-					</Switch>
-				</Main>
 				<footer>
 						@All rights reserved by Junlong
 					</footer>
