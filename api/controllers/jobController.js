5c5cf8e666b982c2333b95f20cd737fff3b0c8a2
Junlong Wang <wjlinnz@gmail.com>
2017-10-25 23:48:28 +1300

    Added getJobs function to retrieve jobs of specific user

--- api/controllers/jobController.js
+++ api/controllers/jobController.js
@@ -1,54 +1,67 @@
-'use strict';
+"use strict";
 
-const Job = require('../models/jobModel');
+const Job = require("../models/jobModel");
 
-function listJobs(req,res, next){
-	Job.listJobs((err, results)=>{
-		if(err){
-			next(err);
-		}
-		res.status(200).json(results);
-	});
+/*
+* Get jobs for certain user
+* */
+function getJobs(req, res, next) {
+  Job.getJobs(req.user.userId, (err, results) => {
+    if (err) {
+      next(err);
+    }
+    res.status(200).json(results);
+  });
 }
 
-
-function singleJob(req, res, next){
-	let id = req.params.app_id;
-	Job.singleJob(id, (err, results)=>{
-		if(err){
-			next(err);
-		}else{
-			res.status(200).json(results);
-		}
-	});
+/*
+* Just for development test purpose
+* */
+function listJobs(req, res, next) {
+  Job.listJobs((err, results) => {
+    if (err) {
+      next(err);
+    }
+    res.status(200).json(results);
+  });
 }
 
-
-function createJob(req, res, next){
-	
-	Job.createJob(req.body, (err, result)=>{
-		if(err){
-			next(err);
-		}else{
-			res.status(201).json(result);
-		}
-	});
+function singleJob(req, res, next) {
+  let id = req.params.app_id;
+  Job.singleJob(id, (err, results) => {
+    if (err) {
+      next(err);
+    } else {
+      res.status(200).json(results);
+    }
+  });
 }
 
+function createJob(req, res, next) {
 
-function deleteJob(req, res, next){
-	Job.deleteJob(req.params.app_id, (err, result)=>{
-		if(err){
-			next(err);
-		}else{
-			res.status(200).json(result);
-		}
-	});
+  Job.createJob(req.body, (err, result) => {
+    if (err) {
+      next(err);
+    } else {
+      res.status(201).json(result);
+    }
+  });
 }
 
-module.exports={
-	ListJobs: listJobs,
-	SingleJob: singleJob,
-	CreateJob: createJob,
-	DeleteJob: deleteJob,
+function deleteJob(req, res, next) {
+  Job.deleteJob(req.params.app_id, (err, result) => {
+    if (err) {
+      next(err);
+    } else {
+      res.status(200).json(result);
+    }
+  });
 }
+
+module.exports = {
+  ListJobs: listJobs,
+  GetJobs: getJobs,
+  SingleJob: singleJob,
+  CreateJob: createJob,
+  DeleteJob: deleteJob,
+};
