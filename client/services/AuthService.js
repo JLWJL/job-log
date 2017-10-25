9127c558a24c8c9f98f2f377c5b4c8fefc5d0650
Junlong Wang <wjlinnz@gmail.com>
2017-10-25 17:50:19 +1300

    Updated 'logout()' with method and body options

--- client/services/AuthService.js
+++ client/services/AuthService.js
@@ -1,97 +1,91 @@
 import jwtService from 'jsonwebtoken';
 
-export default class AuthService{
-	constructor(domain){
-		
-		this.domain = domain || 'http://localhost:3000';
-		this.signup = this.signup.bind(this);
-		this.login = this.login.bind(this);
-		this.logout = this.logout.bind(this);
-		this.fetch = this.fetch.bind(this);
-		this.getFormData = this.getFormData.bind(this);
-		this.isLoggedIn = this.isLoggedIn.bind(this);
+export default class AuthService {
+  constructor(domain) {
 
-	}
+    this.domain = domain || 'http://localhost:3000';
+    this.signup = this.signup.bind(this);
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+    this.fetch = this.fetch.bind(this);
+    this.getFormData = this.getFormData.bind(this);
+    this.isLoggedIn = this.isLoggedIn.bind(this);
 
-	/**
-	 * Collect form data and convert to json format
-	 */
-	getFormData(formElement){
-		let formData = new FormData(formElement);
+  }
 
-		let jsonFormData = {};
-		for(let d of formData){
-			jsonFormData[d[0]] = d[1];
-		}
-		return JSON.stringify(jsonFormData);
-	}
+  /**
+   * Collect form data and convert to json format
+   */
+  getFormData(formElement) {
+    let formData = new FormData(formElement);
 
+    let jsonFormData = {};
+    for (let d of formData) {
+      jsonFormData[d[0]] = d[1];
+    }
+    return JSON.stringify(jsonFormData);
+  }
 
-	signup(formdata){
-		return this.fetch(`${this.domain}/user/auth/signup`, {
-			'method':'POST',
-			'body': formdata
-		})
-	}
+  signup(formdata) {
+    return this.fetch(`${this.domain}/user/auth/signup`, {
+      'method': 'POST',
+      'body': formdata,
+    });
+  }
 
-	
-	login(credential){
-		return this.fetch(`${this.domain}/user/auth/login`, {
-			method:'POST',
-			body: credential
-		})
-		.then(
-			res=>{
-				this.setToken(res.token);
-			}
-		)
-	}
+  login(credential) {
+    return this.fetch(`${this.domain}/user/auth/login`, {
+      method: 'POST',
+      body: credential,
+    }).then(
+        res => {
+          this.setToken(res.token);
+        },
+    );
+  }
 
+  logout() {
+    return this.fetch(`${this.domain}/user/auth/logout`, {
+      method: 'POST',
+      body: null,
+    }).then(
+        res => {
+          localStorage.removeItem('token');
+          return res;
+        },
+    );
+  }
 
-	logout(){
-		return this.fetch(`${this.domain}/user/auth/logout`)
-				.then(
-					res=>{
-						localStorage.removeItem('token', undefined);
-						return res
-					}
-				)
-	}
+  isLoggedIn() {
+    //should verify later on
+    return localStorage.getItem('token');
+  }
 
+  setToken(token) {
+    //Should invoke later on
+    localStorage.setItem('token', token);
+  }
 
-	isLoggedIn(){
-		//should verify later on
-		return localStorage.getItem('token');
-	}
+  fetch(url, options) {
 
+    let init = {
+      'headers': {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+        'X-Authentication': localStorage.getItem('token'),
+      },
+    };
 
-	setToken(token){
-		//Should invoke later on
-		localStorage.setItem('token', token)
-	}
+    let newInit = Object.assign(init, options);
+    return fetch(url, newInit).then(this.checkStatus);
+  }
 
-
-	fetch(url, options){
-
-		let init = {
-			'headers': {
-				'Content-Type':"application/json",
-				'Accept': "application/json, application/xml, text/plain, text/html, *.*",
-				'X-Authentication': localStorage.getItem('token'),
-			},
-		}
-
-		let newInit = Object.assign(init, options)
-		return fetch(url, init).then(this.checkStatus)
-	}
-
-
-	checkStatus(res){
-		if(res.ok){
-			return res.json();
-		}
-		else{
-			throw new Error(`${res.status}: ${res.statusText}`);
-		}
-	}
+  checkStatus(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    else {
+      throw new Error(`${res.status}: ${res.statusText}`);
+    }
+  }
 }
