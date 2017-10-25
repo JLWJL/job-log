54865e8f3ab1ec172dcf7c89d29774f6cabfbb7f
Junlong Wang <wjlinnz@gmail.com>
2017-10-25 23:52:05 +1300

    Added checkUserStatus middleware - Check if user exists before retrieving jobs

--- api/routes/jobRoutes.js
+++ api/routes/jobRoutes.js
@@ -1,18 +1,17 @@
-'use strict';
+"use strict";
 
-const jobController = require('../controllers/jobController');
-const express = require('express');
+const jobController = require("../controllers/jobController");
+const express = require("express");
 const router = express.Router();
 
-const MW = require('./middlewares');
+const MW = require("./middlewares");
 
-router.route('/')
-	.get(MW.checkTokenStatus, jobController.ListJobs)
-	.post(MW.checkTokenStatus, jobController.CreateJob)
+router.route("/").
+    get(MW.checkTokenStatus, MW.checkUserStatus, jobController.GetJobs).
+    post(MW.checkTokenStatus, jobController.CreateJob);
 
-router.route('/:app_id')
-	.get(MW.checkTokenStatus, jobController.SingleJob)
-	.delete(MW.checkTokenStatus, jobController.DeleteJob)
+router.route("/:app_id").
+    get(MW.checkTokenStatus, jobController.SingleJob).
+    delete(MW.checkTokenStatus, jobController.DeleteJob);
 
-
-module.exports=router;
+module.exports = router;
