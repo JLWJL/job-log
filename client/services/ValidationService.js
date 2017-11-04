e58a5d8073534e3850080e4d7451418084a1423e
Junlong Wang <wjlinnz@gmail.com>
2017-11-04 23:38:27 +1300

    Added ValidationService to validate form data - Regex for email - Lenght of other input > 1

---
+++ client/services/ValidationService.js
@@ -0,0 +1,21 @@
+/**
+ * Validate email from user input. Email is:
+ * required, match email format, no space
+ * @param {String} email
+ */
+export function validateEmail(email) {
+	let regex = /^\w+@\w+?(\.[a-zA-Z]{2,}){1,2}$/;
+	return {"result": regex.test(email), "message": "Invalid email"}
+}
+
+/**
+ * Validate length of input
+ * @param {String} input
+ * @param {String} option: Name of the input so that prompt can be specific
+ * @returns {{result: boolean, message: string}}
+ */
+export function validateLength(input, option) {
+	let placeholder = (typeof option === "string" && option.length>0) ? option : "Input";
+	return ({"result": input.length >= 1, "message": placeholder+" at least 1 character"});
+}
+
