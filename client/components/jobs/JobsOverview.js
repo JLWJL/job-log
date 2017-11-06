6702c9a9dc552549d9fe426b12eda57f7c296dd7
Junlong Wang <wjlinnz@gmail.com>
2017-11-06 13:39:36 +1300

    Renamed JobDetails to UpdateJob and added Update Job feature - Renamed reference in other files as well - Simple validation - Cancel button to back viewing all jobs

--- client/components/jobs/JobsOverview.js
+++ client/components/jobs/JobsOverview.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import DisplayAllJobs from './DisplayAllJobs';
-import JobDetails from './JobDetails';
+import JobDetails from './UpdateJob';
 import NewJob from './NewJob';
 import NotFoundPage from '../NotFoundPage';
 
