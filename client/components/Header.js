c1fff50d6cdd525b51414de5f65086e1b94bce00
Junlong Wang <wjlinnz@gmail.com>
2017-11-08 15:43:45 +1300

    Modified header layout and colour scheme - Imported jQuery and Bootstrap js

--- client/components/Header.js
+++ client/components/Header.js
@@ -6,10 +6,10 @@ import {Link, NavLink} from 'react-router-dom';
 //export default class Header extends React.Component{
 export default function(props){
 
-	
+
 		return(
 			<header>
-						<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+						<nav className="navbar navbar-expand-lg navbar-light">
 						  <a className="navbar-brand" href="#">Logo</a>
 						  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
 						    <span className="navbar-toggler-icon"></span>
